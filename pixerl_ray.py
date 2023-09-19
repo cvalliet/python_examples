54e348908dfe6248a6ea3484441ba66be6cd6687
cvalliet <cvalliet@users.noreply.github.com>
2023-09-19 09:57:43 +0200

    Update pixel_ray.py

--- pixerl_ray.py
+++ pixerl_ray.py
@@ -32,19 +32,27 @@ Map = [
 
 # FUNCTIONs -----------------------------------------------------------------------------------------------------------
 
+def vector2_length(v):
+    return math.sqrt(v[0]*v[0] + v[1]*v[1])
+
+
 def vector2_add(v1, v2):
     return v1[0] + v2[0], v1[1] + v2[1]
+
 
 def vector2_substract(v1, v2):
     return v1[0] - v2[0], v1[1] - v2[1]
 
-def vector2_multiply(v1, scalar):
-    return v1[0] * scalar, v1[1] * scalar
 
-def vector2_rotate(v1, angle):
-    return v1[0] * math.cos(angle) - v1[1] * math.sin(angle), v1[0] * math.sin(angle) + v1[1] * math.cos(angle)
+def vector2_multiply(v, scalar):
+    return v[0] * scalar, v[1] * scalar
 
-def draw_map(map, map_size, block_size):
+
+def vector2_rotate(v, angle):
+    return v[0] * math.cos(angle) - v[1] * math.sin(angle), v[0] * math.sin(angle) + v[1] * math.cos(angle)
+
+
+def map_draw(map, map_size, block_size):
     surface = pygame.Surface((block_size*map_size, block_size*map_size))
     for row in range(map_size):
         for col in range(map_size):
@@ -53,12 +61,20 @@ def draw_map(map, map_size, block_size):
 
     return surface
 
-def check_wall(position, map, map_size, block_size):
+
+def map_is_empty(map, map_size, block_size, position):
     x, y = int(position[0] // block_size), int(position[1] // block_size)
     return map[y][x] == ' '
 
 
 # CLASSEs -------------------------------------------------------------------------------------------------------------
+
+
+class Player:
+    def __init__(self, position, direction):
+        self._position = position
+        self._direction = direction
+
 
 class PygameApplication:
     def __init__(self):
@@ -79,12 +95,18 @@ class PygameApplication:
         ROTATION_SPEED = 0.05
         MOVE_SPEED = 0.3
 
-        origin = 0, 0 #screen_size[0] // 2, screen_size[1] // 2
+        # origin vector
+        origin = 0, 0
+        # middle vector
+        middle = screen_size[0] // 2, screen_size[1] // 2
+        # position vector
         position = 21, 24
-        direction = 0, 1
+        # direction vector
+        direction = -1, 0
+        # plane vector
         plane = 0, 0.66
-        move = 0, 0
-        map_surface = draw_map(Map, 10, 16)
+
+        map_surface = map_draw(Map, 10, 16)
 
         while (self._running):
             self._screen.fill('black')
@@ -110,42 +132,29 @@ class PygameApplication:
                 direction = vector2_rotate(direction, -ROTATION_SPEED)
                 plane = vector2_rotate(plane, -ROTATION_SPEED)
 
-            move = direction
-
             if keys[K_UP]:
-                expected_move = vector2_multiply(move, MOVE_SPEED)
-                expected_position = vector2_add(position, expected_move)
-                if check_wall(expected_position, Map, 10, 16):
+                expected_position = vector2_add(position, vector2_multiply(direction, MOVE_SPEED))
+                if map_is_empty(Map, 10, 16, expected_position):
                     position = expected_position
-                    move = expected_move
 
             if keys[K_DOWN]:
-                expected_move = vector2_multiply(move, -MOVE_SPEED)
-                expected_position = vector2_add(position, expected_move)
-                if check_wall(expected_position, Map, 10, 16):
+                expected_position = vector2_add(position, vector2_multiply(direction, -MOVE_SPEED))
+                if map_is_empty(Map, 10, 16, expected_position):
                     position = expected_position
-                    move = expected_move
 
-            move = direction
-            move = vector2_multiply(move, 8)
-            move = vector2_add(move, position)
+            pygame.draw.circle(self._screen, 'red', position, 5.)
 
-            start, end = vector2_add(position, origin), vector2_add(move, origin)
+            # position + direction * 10 => display direction vector with a length or 10 pixels
+            end = vector2_add(vector2_multiply(direction, 10), position)
+            pygame.draw.line(self._screen, 'yellow', position, end)
 
-            pygame.draw.circle(self._screen, 'red', start, 5.)
-            pygame.draw.line(self._screen, 'yellow', start, end)
-
-            #for x in range(0, 400, 2):
-            x = 0
-            camera = 2.0 * x / 400 - 1.0
-            ray = position
-            ray_direction = direction[0] + plane[0] * camera + .000000000000001, direction[1] + plane[1] - camera + .000000000000001
-            ray_map = int(position[0]), int(position[1])
-            print(ray_direction)
-            xy_square = ray_direction[0] * ray_direction[0], ray_direction[1] * ray_direction[1]
-
-            delta_distance = math.sqrt(1.0 + xy_square[1] / xy_square[0]), math.sqrt(1.0 + xy_square[0] / xy_square[1])
-            print(delta_distance)
+            # FOV
+            WIDTH = 320
+            for column in range(0, WIDTH):
+                x = 2 * column / WIDTH - 1
+                ray = vector2_add(direction, vector2_multiply(plane, x))
+                end = vector2_add(vector2_multiply(ray, 20), position)
+                pygame.draw.line(self._screen, 'magenta', position, end)
 
             pygame.transform.scale2x(self._screen, self._window)
 
@@ -154,7 +163,8 @@ class PygameApplication:
             self._clock.tick(fps)
             self._frame += 1
 
-            pygame.display.set_caption('PIXEL ART - Frame : #%04d' % (self._frame % 1000))
+            pygame.display.set_caption(f'PIXEL ART - Frame : #{self._frame % 1000:04d}')
+
 
 # VARIABLEs -----------------------------------------------------------------------------------------------------------
 
